80f0f99ffbc0e20ee48fb97250be331622162ff1
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-09-04 12:08:16 +0200

    Add validation for article title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,17 @@ class ArticlesController < ApplicationController
 
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
